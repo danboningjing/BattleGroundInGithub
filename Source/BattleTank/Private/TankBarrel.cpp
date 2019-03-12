ab656d61cbe73afb67af0b856e6f7f1a018e70cf
DanBoNingJing <mingbaolin97@qq.com>
2019-03-12 22:57:03 +0800

    Course 158

--- Source/BattleTank/Private/TankBarrel.cpp
+++ Source/BattleTank/Private/TankBarrel.cpp
@@ -7,7 +7,6 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 	// Move the barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
 	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed,-1,1);
-	UE_LOG(LogTemp, Warning, TEXT("Rotator.Pitch: %f"),RelativeSpeed);
 	auto ElevationChange = RelativeSpeed * MaxDegreesPerSeconds * GetWorld()->DeltaTimeSeconds;
 	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
 	auto Elevation = FMath::Clamp<float>(RawNewElevation,MinElevationDegrees,MaxElevationDegrees);
--- Source/BattleTank/Private/TankTurret.cpp
+++ Source/BattleTank/Private/TankTurret.cpp
@@ -6,8 +6,7 @@ void UTankTurret::Rotate(float RelativeSpeed)
 {
 	// Move the barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
-	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
-	UE_LOG(LogTemp, Warning, TEXT("Rotator.Pitch: %f"), RelativeSpeed);
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);	
 	auto RotationChange = RelativeSpeed * MaxDegreesPerSeconds * GetWorld()->DeltaTimeSeconds;
 	auto Rotation = RelativeRotation.Yaw + RotationChange;
 	SetRelativeRotation(FRotator(0, Rotation, 0));
